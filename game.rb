75bc65ff0f57dec789f4be47fbf1b6b5e3a8fb1a
rasmus-hulthe-nti-johanneberg <rasmus.hulthe@elev.ga.ntig.se>
2021-10-25 09:25:09 +0200

    WE HAVE A GRID

--- game.rb
+++ game.rb
@@ -2,54 +2,43 @@ require 'gosu'
 
 class Intro < Gosu::Window
     def initialize
-        @window_size = [1000, 800]
+        @window_size = [1000, 1000]
         super @window_size[0], @window_size[1]
         self.caption = 'Spiders In Your Favorite Shoes? Just Leeave Them Be, Couse Theyre More Scared Of You.'
         @mid = @window_size[0] / 2
-        @frame = 0
-        @image = Gosu::Image.new("krister.PNG")
-        @krister_size = [940, 920]#[1360, 720] #
-        @krister_size_percent = [0.5, 0.5]
-        @krister_coords = [0, 0]
-        @krister_speed = [5, 5]
-        @dir = [true, true]
+        @grid = []
+        @grid_dist_x = 40
+        @grid_dist_y = 40
+        @y_offset = 100
+        @aqua = Gosu::Color.argb(0xff_00ffff)
+        @green = Gosu::Color.argb(0xff_00ff00)
+        @fushia = Gosu::Color.argb(0xff_ff00ff)
+        @yellow_block = Gosu::Color.argb(0xff_ffff00)
+        for i in 0..10 do
+            @grid.append([@mid+@grid_dist_x*(i-5), @y_offset + @grid_dist_y*20, @mid+@grid_dist_x*(i-5), @y_offset])
+        end
+        for i in 0..20 do
+            @grid.append([@mid-@grid_dist_x*5, @y_offset + i*@grid_dist_y, @mid+@grid_dist_x*5, @y_offset + i*@grid_dist_y])
+        end
+        #p @grid
     end
 
     def update
+        
+    end
 
-        if @dir[0]
-            if ((@window_size[0]-(@krister_size[0]*@krister_size_percent[0])) - @krister_coords[0]) > 0
-                @krister_coords[0] += @krister_speed[0]
-            else
-                @dir[0] = false
-            end
-        else
-            if @krister_coords[0] != 0
-                @krister_coords[0] -= @krister_speed[0]
-            else
-                @dir[0] = true
-            end
-        end
-
-
-        if @dir[1]
-            if ((@window_size[1]-(@krister_size[1]*@krister_size_percent[1])) - @krister_coords[1]) > 0
-                @krister_coords[1] += @krister_speed[1]
-            else
-                @dir[1] = false
-            end
-        else
-            if @krister_coords[1] > 0
-                @krister_coords[1] -= @krister_speed[1]
-            else
-                @dir[1] = true
-            end
-        end
+    def draw_grid
+        @grid.each{|line|
+            self.draw_line(line[0], line[1],@fushia, line[2], line[3], @green)    
+        }
+        
     end
 
     def draw
+        draw_grid()
+        #self.draw_line(100,0,@fushia, 500, 1000, @green)
         #p @frame
-        @image.draw(@krister_coords[0], @krister_coords[1], 0, @krister_size_percent[0], @krister_size_percent[1])
+        #@image.draw(@krister_coords[0], @krister_coords[1], 0, @krister_size_percent[0], @krister_size_percent[1])
     end
 end
 
